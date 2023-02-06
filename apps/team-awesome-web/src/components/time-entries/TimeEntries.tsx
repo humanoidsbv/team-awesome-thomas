01eecebc55f08ea0aa811387b03b2d4bcddf2d9a
Thomas van den Berg <contact@bergtop.io>
2023-02-06 17:50:28 +0100

    Add mock data and data mapping

--- apps/team-awesome-web/src/components/time-entries/TimeEntries.tsx
+++ apps/team-awesome-web/src/components/time-entries/TimeEntries.tsx
@@ -1,13 +1,48 @@
 import * as Styled from "./TimeEntries.styled";
+import { useState } from "react";
 import { TimeEntry } from "../time-entry/";
 import { TimeEntryHeader } from "../time-entry-header/";
+import { Button } from "../button/";
+import mockTimeEntries from "../../fixtures/mock-time-entries";
 
-export const TimeEntries = () => (
-  <Styled.TimeEntries>
-    <TimeEntryHeader />
-    <TimeEntry />
-    <TimeEntryHeader />
-    <TimeEntry />
-    <TimeEntry />
-  </Styled.TimeEntries>
-);
+export const TimeEntries = () => {
+  const [timeEntries, setTimeEntries] = useState(mockTimeEntries);
+  const handleClick = () => {
+    setTimeEntries([
+      ...timeEntries,
+      {
+        id: Math.random() + 2,
+        client: "Port of R'dam",
+        startTimestamp: "2022-09-26T16:00:00.000Z",
+        stopTimestamp: "2022-09-26T18:00:00.000Z",
+      },
+    ]);
+  };
+
+  const currentDate = new Date();
+  const currentDayOfMonth = currentDate.getDate();
+  const currentDay = currentDate.getDay();
+  const currentMonth = currentDate.getMonth();
+
+  return (
+    <Styled.TimeEntries>
+      {timeEntries.map((timeEntry) => {
+        return (
+          <>
+            <TimeEntryHeader
+              startDate={timeEntry.startTimestamp}
+              endDate={timeEntry.stopTimestamp}
+            />
+            <TimeEntry
+              key={timeEntry.id}
+              client={timeEntry.client}
+              startDate={timeEntry.startTimestamp}
+              endDate={timeEntry.stopTimestamp}
+            />
+          </>
+        );
+      })}
+      <Button onClick={handleClick}>Add time entry</Button>
+    </Styled.TimeEntries>
+  );
+};
--- apps/team-awesome-web/src/components/time-entry/TimeEntry.tsx
+++ apps/team-awesome-web/src/components/time-entry/TimeEntry.tsx
@@ -1,12 +1,41 @@
 import * as Styled from "./TimeEntry.styled";
+import { useState, ReactNode } from "react";
 
-export const TimeEntry = () => (
-  <Styled.TimeEntry>
-    <div>Port of Rotterdam</div>
-    <Styled.Times>
-      <Styled.TimeRange>09:00 - 17:00</Styled.TimeRange>
-      <Styled.Duration>08:00</Styled.Duration>
-    </Styled.Times>
-    <Styled.DeleteIcon />
-  </Styled.TimeEntry>
-);
+interface EntryProps {
+  client: string;
+  endDate: string;
+  key: number;
+  startDate: string;
+  onClick?: (event: React.MouseEvent<HTMLButtonElement>) => void;
+}
+
+export const TimeEntry = ({ key, client, endDate, startDate }: EntryProps) => {
+  const dateStart = new Date(startDate);
+  const dateEnd = new Date(endDate);
+  const timeDuration = dateEnd.getTime() - dateStart.getTime();
+  const durationString = `${Math.floor(timeDuration / 1000 / 3600)}:${(
+    "0" + ((timeDuration / 1000 / 60) % 60).toString()
+  ).slice(-2)}`;
+  const entryTimeStartString = dateStart.toLocaleTimeString("nl-NL", {
+    hour: "2-digit",
+    minute: "2-digit",
+  });
+  const entryTimeEndString = dateEnd.toLocaleTimeString("nl-NL", {
+    hour: "2-digit",
+    minute: "2-digit",
+  });
+
+  console.log(Math.floor(timeDuration / 1000 / 60 / 60));
+  return (
+    <Styled.TimeEntry>
+      <div>{client}</div>
+      <Styled.Times>
+        <Styled.TimeRange>
+          {entryTimeStartString} - {entryTimeEndString}
+        </Styled.TimeRange>
+        <Styled.Duration>{durationString}</Styled.Duration>
+      </Styled.Times>
+      <Styled.DeleteIcon />
+    </Styled.TimeEntry>
+  );
+};
