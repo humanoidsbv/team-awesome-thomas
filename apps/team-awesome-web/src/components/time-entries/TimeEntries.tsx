ad5d7fc3b335d56d2fa6108a1875dcf523e6f769
Thomas van den Berg <contact@bergtop.io>
2023-02-20 16:03:49 +0100

    Add functionality to calculate time for time entry

--- apps/team-awesome-web/src/components/time-entries/TimeEntries.tsx
+++ apps/team-awesome-web/src/components/time-entries/TimeEntries.tsx
@@ -57,6 +57,16 @@ export const TimeEntries = ({ ...props }: TimeEntriesProps) => {
   };
 
   const handleChange = ({ target }: React.ChangeEvent<HTMLInputElement>) => {
+    if (target.name === "from") {
+      const newStartTimestamp = new Date(`${newTimeEntry.date}T${target.value}:00`);
+      setNewTimeEntry({ ...newTimeEntry, startTimestamp: newStartTimestamp.toISOString() });
+      return;
+    }
+    if (target.name === "to") {
+      const newStopTimestamp = new Date(`${newTimeEntry.date}T${target.value}`);
+      setNewTimeEntry({ ...newTimeEntry, stopTimestamp: newStopTimestamp.toISOString() });
+      return;
+    }
     setNewTimeEntry({ ...newTimeEntry, [target.name]: target.value });
   };
 
