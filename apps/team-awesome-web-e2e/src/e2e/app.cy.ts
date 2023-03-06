7c1ab1d73fa9594182a8d89e1779ca6bd71cddad
Thomas van den Berg <contact@bergtop.io>
2023-03-06 14:38:28 +0100

    Refactor existing Cypress test code

--- apps/team-awesome-web-e2e/src/e2e/app.cy.ts
+++ apps/team-awesome-web-e2e/src/e2e/app.cy.ts
@@ -3,7 +3,7 @@ import { getGreeting } from "../support/app.po";
 describe("team-awesome-web", () => {
   beforeEach(() => cy.visit("/"));
 
-  it("should display welcome message", () => {
+  it("Should display welcome message", () => {
     // Custom command example, see `../support/commands.ts` file
     cy.login("my-email@something.com", "myPassword");
 
@@ -15,24 +15,26 @@ describe("team-awesome-web", () => {
 describe("Routing", () => {
   beforeEach(() => cy.visit("/"));
 
+  const teamMembersUrl = "/team-members";
+  const timeEntriesUrl = "/timesheets";
+
   it("Navigates to other pages", () => {
-    const headerNav = cy.get("header");
+    cy.get("header").get(`[href="${teamMembersUrl}"]`).click();
+    cy.url().should("include", teamMembersUrl);
 
-    headerNav.get('[href="/team-members"]').click();
-
-    headerNav.get('[href="/timesheets"]').click();
+    cy.get("header").get(`[href="${timeEntriesUrl}"]`).click();
+    cy.url().should("include", timeEntriesUrl);
   });
 
   it("Navigates to other pages, but on mobile", () => {
     cy.viewport(375, 815);
 
-    const headerNav = cy.get("header");
-    const navButtonClick = () => headerNav.get('[data-cy="nav-button"]').click({ timeout: 1000 });
+    cy.get("header").get('[data-cy="nav-button"]').click();
+    cy.get("header").get(`[href="${teamMembersUrl}"]`).click();
+    cy.url().should("include", teamMembersUrl);
 
-    navButtonClick();
-    headerNav.get('[href="/team-members"]').click();
-
-    navButtonClick();
-    headerNav.get('[href="/timesheets"]').click();
+    cy.get("header").get('[data-cy="nav-button"]').click();
+    cy.get("header").get(`[href="${timeEntriesUrl}"]`).click();
+    cy.url().should("include", timeEntriesUrl);
   });
 });
