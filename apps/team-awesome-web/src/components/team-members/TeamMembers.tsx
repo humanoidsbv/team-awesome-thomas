ba8273191a3771ba09c940b7fcd217390e9b3ee4
Thomas van den Berg <contact@bergtop.io>
2023-03-07 09:59:44 +0100

    Add Team Member Gql request

--- apps/team-awesome-web/src/components/team-members/TeamMembers.tsx
+++ apps/team-awesome-web/src/components/team-members/TeamMembers.tsx
@@ -1,4 +1,5 @@
 import { useContext, useEffect, useState } from "react";
+import { useQuery } from "@apollo/client";
 
 import * as Styled from "./TeamMembers.styled";
 import { TeamMember } from "../team-member";
@@ -6,12 +7,26 @@ import * as Types from "../../types";
 import { StoreContext } from "../store-context";
 import { Select } from "../forms/select";
 
+// GraphQL import
+import { GET_TEAM_MEMBERS } from "../../graphql/team-members/queries";
+
 interface TeamMembersProps {
   errorMessage?: string;
   teamMembers: Types.TeamMember[];
 }
 
 export const TeamMembers = ({ ...props }: TeamMembersProps) => {
+  const { data } = {};
+
+  // GraphQL shenanigans
+  const { data: teamMemberData } = useQuery(GET_TEAM_MEMBERS, {
+    pollInterval: 2000,
+  });
+
+  const { allTeamMembers = {} } = teamMemberData;
+
+  console.log(allTeamMembers);
+
   const { teamMembers, setTeamMembers } = useContext(StoreContext);
 
   const [sortedTeamMembers, setSortedTeamMembers] = useState<Types.TeamMember[]>(props.teamMembers);
@@ -26,14 +41,7 @@ export const TeamMembers = ({ ...props }: TeamMembersProps) => {
 
   return (
     <Styled.TeamMembers>
-      <Styled.Actions>
-        <Select
-          sortList="teamMembers"
-          sortArray={teamMembers}
-          setSortedResults={setSortedTeamMembers}
-          direction
-        />
-      </Styled.Actions>
+      <Styled.Actions>{/* <Select sortList="teamMembers" direction /> */}</Styled.Actions>
       {sortedTeamMembers.map((teamMember) => (
         <TeamMember key={teamMember.id} teamMember={teamMember} />
       ))}
